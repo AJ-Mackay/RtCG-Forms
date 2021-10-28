278f97bcfac04902418922a2b490ccc658505e00
Paul Mackay <paul.m.mackay85@gmail.com>
2021-10-28 19:07:15 +0100

    Adding Basic Validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState('');
 
   const nameInputChangeHandler = (event) => {
@@ -10,10 +11,16 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
+    if (enteredName.trim() === '') {
+      return;
+    }
+
     console.log(enteredName);
 
-    // nameInputRef.current.value = ''; // NOT IDEAL, DON'T MANIPULATE THE DOM
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
 
+    // nameInputRef.current.value = ''; => NOT IDEAL, DON'T MANIPULATE THE DOM
     setEnteredName('');
   };
 
@@ -22,6 +29,7 @@ const SimpleInput = (props) => {
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
         <input
+          ref={nameInputRef}
           type="text"
           id="name"
           onChange={nameInputChangeHandler}
